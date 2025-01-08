0af18d427abc29b57143cf9531dd4393c1e5f2fa
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2025-01-09 00:12:58 +0200

    commit

---
+++ Python/2011.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        X = 0
+        for op in operations:
+            if op == "++X" or op == "X++":
+                X += 1
+            elif op == "--X" or op == "X--":
+                X -= 1
+        return X
